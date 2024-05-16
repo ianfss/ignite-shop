496d1825e79c6c583fc7e9207a404aabe32715c9
Ian <ssianfilipe@gmail.com>
2024-05-15 22:14:51 -0300

    format price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,12 +34,7 @@ export default function Home({ products }: HomeProps) {
 
             <footer>
               <strong>{product.name}</strong>
-              <span>
-                {product.price.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })}
-              </span>
+              <span>{product.price}</span>
             </footer>
           </Product>
         )
@@ -60,7 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     }
   })
 
